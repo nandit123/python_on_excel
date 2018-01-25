62e6e2a645ed540f11f7ac2ebc63058748915a77
Nandit Mehra <nandit.mehra.15csc@bml.edu.in>
2018-01-25 11:08:22 +0530

    code2 file for numpy analysis of excel added

--- code_numpy.py
+++ code_numpy.py
@@ -1,25 +1,26 @@
+import numpy
 import xlrd
 import xlwt
 from xlutils.copy import copy
 
-workbook = xlrd.open_workbook('data.xlsx')
+workbook = xlrd.open_workbook('data2.xlsx')
 worksheet = workbook.sheet_by_index(0)
 
 workbook2 = copy(workbook)
 worksheet2 = workbook2.get_sheet(0)
 
 
+#for internal component 1
+
 for j in range(5, 10):
-	count = 0
-	total = 0
+	l = []
 	for i in range(2, 63): # i goes from 2 to 63
 		if worksheet.cell(i, j).value != xlrd.empty_cell.value:	
-			total += float(worksheet.cell(i, j).value)
-			count+=1
-	average = total/count
-	print(average)
-
+			l.append(float(worksheet.cell(i, j).value))
+	a = numpy.array(l)
+	m = numpy.median(a)
+	print('median is ',m)
 	for i in range(2,63):
 		if worksheet.cell(i, j).value == xlrd.empty_cell.value:
-			worksheet2.write(i, j, average)
-workbook2.save('data.xlsx')
+			worksheet2.write(i, j, m)
+workbook2.save('data2.xlsx')
